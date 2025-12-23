c7bfa8917149a817234f0a63b90e6cf97d8765fa
mrbuba <wwpptew101@gmail.com>
2025-12-23 22:20:11 +0700

    feat: add ans ex1-7

--- ex-2.js
+++ ex-2.js
@@ -14,4 +14,8 @@ let studentScores = {
 
 // Start coding here
 
+let newStudentProfile = {
+  ...studentProfile,
+  scores: { ...studentProfile.scores, ...studentScores }, //สร้าง key scores: แล้วรวม 2 obj ไว้ในนั้น
+};
 console.log(newStudentProfile);
